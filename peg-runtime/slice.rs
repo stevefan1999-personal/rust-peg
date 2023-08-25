dcc8fb335ec768b3d98c241581b9d971b79671df
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2023-08-26 00:20:40 +0800

    Update slice.rs

--- peg-runtime/slice.rs
+++ peg-runtime/slice.rs
@@ -26,6 +26,17 @@ impl<'input, T: 'input + Copy> ParseElem<'input> for [T] {
     }
 }
 
+impl<'input, T: 'input + Clone> ParseElem<'input> for [T] {
+    type Element = T;
+
+    fn parse_elem(&'input self, pos: usize) -> RuleResult<T> {
+        match self[pos..].first() {
+            Some(c) => RuleResult::Matched(pos + 1, c.clone()),
+            None => RuleResult::Failed,
+        }
+    }
+}
+
 impl ParseLiteral for [u8] {
     fn parse_string_literal(&self, pos: usize, literal: &str) -> RuleResult<()> {
         let l = literal.len();
