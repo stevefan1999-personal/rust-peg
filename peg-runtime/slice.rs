8d83464960525fcd0ffc201fa28e21bb7c192391
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2023-08-26 00:41:03 +0800

    Update slice.rs

--- peg-runtime/slice.rs
+++ peg-runtime/slice.rs
@@ -54,3 +54,44 @@ impl<'input, T: 'input> ParseSlice<'input> for [T] {
         &self[p1..p2]
     }
 }
+
+pub struct SliceByRef<'input, T>(pub &'input [T]);
+
+impl<'input, T> From<&'input [T]> for SliceByRef<'input, T> {
+    fn from(value: &'input [T]) -> Self {
+        Self(value)
+    }
+}
+
+impl<'input, T> Parse for SliceByRef<'input, T> {
+    type PositionRepr = usize;
+    fn start(&self) -> usize {
+        0
+    }
+
+    fn is_eof(&self, pos: usize) -> bool {
+        pos >= self.0.len()
+    }
+
+    fn position_repr(&self, pos: usize) -> usize {
+        pos
+    }
+}
+
+impl<'input, T: 'input> ParseElem<'input> for SliceByRef<'input, T> {
+    type Element = &'input T;
+
+    fn parse_elem(&'input self, pos: usize) -> RuleResult<&'input T> {
+        match self.0[pos..].first() {
+            Some(c) => RuleResult::Matched(pos + 1, c),
+            None => RuleResult::Failed,
+        }
+    }
+}
+
+impl<'input, T: 'input> ParseSlice<'input> for SliceByRef<'input, T> {
+    type Slice = &'input [T];
+    fn parse_slice(&'input self, p1: usize, p2: usize) -> &'input [T] {
+        &self.0[p1..p2]
+    }
+}
